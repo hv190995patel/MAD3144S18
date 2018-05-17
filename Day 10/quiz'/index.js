7de0ebe9d354c1cc32f50d63f4c930464f24d6c5
Harsh Patel <35268429+hv190995patel@users.noreply.github.com>
2018-05-17 11:57:11 -0400

    Add files via upload

---
+++ Day 10/quiz'/index.js
@@ -0,0 +1,27 @@
+var questionBank =
+  [
+    {
+     "question": "Who's my favorite Avenger?",
+     "answers": {
+         "a":"Iron Man",
+         "b":"Thor"
+       },
+     "correct":"b",
+    },
+    {
+     "question": "What's word do I use the most?",
+     "answers": {
+       "a":"Awesome!",
+       "b":"Bakwas!"
+      },
+     "correct":"a",
+    },
+    {
+     "question": "What's my favorite programming language?",
+     "answers": {
+       "a":"Python",
+       "b":"Swift"
+      },
+     "correct":"a",
+    }
+  ]
